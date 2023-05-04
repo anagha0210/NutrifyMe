1ef50297a0d731aa0adc98092a09e744cb7440b4
ahmed.nawaz <ahmed.nawaz@ncnet.com>
2023-05-04 15:51:41 +0500

    set the schema for food recommendation API

--- backend/FoodRecommender/models.py
+++ backend/FoodRecommender/models.py
@@ -24,3 +24,64 @@ class ProductSchema(ma.Schema):
 # Init schema
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
+
+
+
+
+
+# User Class/Model
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+  email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+  password = db.Column(db.String(length=60), nullable=False)
+
+  def __init__(self, name, email_address,password ):
+    self.name = name
+    self.email_address=email_address
+    self.password=password
+
+  def check_password_correction(self, attempted_password):
+    return self.password== attempted_password
+
+# User Schema
+class UserSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'name', 'email_address', 'password')
+
+# Init schema
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+
+# Ingredient Class/Model
+class Ingredient(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+    # foreign key
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+    nullable=False)
+  user = db.relationship('User',
+    backref=db.backref('ingredients', lazy=True))
+#   def __init__(self, name, email_address,password ):
+#     self.name = name
+#     self.email_address=email_address
+#     self.password=password
+
+
+# Ingredient Schema
+# class UserSchema(ma.Schema):
+#   class Meta:
+#     fields = ('id', 'name', 'email_address', 'password')
+
+# # Init schema
+# user_schema = UserSchema()
+# users_schema = ProductSchema(many=True)
+
+
+
+
+
+
+
--- backend/FoodRecommender/routes.py
+++ backend/FoodRecommender/routes.py
@@ -1,15 +1,99 @@
+from re import I
 from flask import jsonify
 from flask import request
 from FoodRecommender import app 
 from FoodRecommender.models import Product,product_schema,products_schema
+from FoodRecommender.models import User,user_schema, users_schema
+from FoodRecommender.models import Ingredient
 from FoodRecommender import db
-
-# For jwt token
-
-
+from sqlalchemy.orm import joinedload
 @app.route('/', methods=['GET'])
 def check_api():
   return "api is running"
+
+
+# ///////////////////////////////////////////////////////////////// User Routes
+# Register a User
+@app.route('/register', methods=['POST'])
+def register_user():
+  name = request.json['name']
+  email_address = request.json['emailAddress']
+  password = request.json['password']
+  attempted_user = User.query.filter_by(email_address=email_address ).first()
+  if(attempted_user):
+    return jsonify("Failed! Email is already in use!"),404
+
+  new_user = User(name, email_address, password)
+  db.session.add(new_user)
+  db.session.commit()
+  return user_schema.jsonify(new_user)
+
+# Sign in
+@app.route('/signIn', methods=['POST'])
+def signIn_user():
+ email_address = request.json['emailAddress']
+ password = request.json['password']
+ attempted_user = User.query.filter_by(email_address=email_address ).first()
+ if attempted_user and attempted_user.check_password_correction(attempted_password=password ):
+    return user_schema.jsonify(attempted_user)
+ else:
+    return jsonify("Log in failed!"),404
+
+
+# Get All users
+@app.route('/users', methods=['GET'])
+def get_users():
+  all_users = User.query.all()
+  result = users_schema.dump(all_users)
+  return jsonify(result)
+
+#//////////////////////////////////////////////////////////////////// Ingredient routes
+@app.route('/addProfile', methods=['POST'])
+def add_profile():
+ userId = request.json['userId']
+ Ingredients = request.json['ingredients']
+ currentUser = User.query.filter_by(id=userId ).first()
+ print("ingredients are", Ingredients, "and user is ", currentUser)
+ try:
+    for ingredient in Ingredients:
+        # Post(title='Hello Python!', body='Python is pretty cool', category=py)    
+        p=Ingredient(name=ingredient,user=currentUser)    
+        currentUser.ingredients.append(p)
+        db.session.add(currentUser)
+    db.session.commit()
+
+    return jsonify(Ingredients),200
+ except:
+    return jsonify("failed"),4004
+
+# Get Profile
+@app.route('/profile/<id>', methods=['GET'])
+def get_profile(id):
+    query = User.query.options(joinedload('ingredients'))
+    for user in query:
+        print (user, user.ingredients)
+    return jsonify('profile extracted')
+#   product = User.query.get(id)
+#   return product_schema.jsonify(product)
+
+#//////////////////////////////////////////////////////////////////// Product Routes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Create a Product
